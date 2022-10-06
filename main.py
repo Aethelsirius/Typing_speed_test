9fb774bed16874383e738b6fb16f8881dfb170ec
Aethelsirius <dan.szasz0@gmail.com>
2022-10-06 19:42:53 +0200

    Changing library to PyQt + working stacked widgets

--- main.py
+++ main.py
@@ -1,86 +1,70 @@
-#import tkinter as tk
-from tkinter import *
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QStackedWidget, QPushButton
+import sys
 import time
 import random
 import threading
 
-
-class App(Tk):
+class MainWindow(QMainWindow):
     def __init__(self):
-        Tk.__init__(self)
-        #self.root = Tk()
-        #self.root.title("Typing speed app")
-        #self.root.geometry("1280x720")
+        QMainWindow.__init__(self)
+        self.setWindowTitle("Typing Speed Test App")
+        self.setMinimumSize(600,400)
 
-        #self.root.rowconfigure(0, weight=1)
-        #self.root.columnconfigure(0, weight=1)
+        global stacked
+        stacked = QStackedWidget(self)
+        self.setCentralWidget(stacked)
+        stacked.addWidget(Menu())
+        stacked.addWidget(TS_Test())
+        stacked.addWidget(Statistics())
 
-        #self.frame1 = Frame(self.root, bg="red")
-        #self.frame2 = Frame(self.frame1, bg="green")
-        #self.frame3 = Frame(self.root, bg="blue")
+        #self.stacked.setCurrentIndex(1)
 
-        #self.text = open("paragraphs.txt", "r").read().split("BREAK")
-        
-        #self.sample_label = Label(self.frame1, text=random.choice(self.text))
-        #self.sample_label.place(x=100, y=200)
-        #self.sample_label.grid(row=2, column=0)
-        
-        
-        def test2():
-            print('Ahoj')
-            self.frame4 = Frame(self.root, bg="green")
-            self.frame2.pack(fill="both", expand=True)
-            
 
-        #self.butt1 = Button(self.frame1, text='Test', command=test2)
-        #self.butt1.place(x=600,y=550)
-        #self.butt1.grid()
+class Menu(QWidget):
+    def __init__(self):
+        QWidget.__init__(self) 
+        self.initUI()
+    
+    def initUI(self):
+        self.button_test = QPushButton(self)
+        self.button_graph = QPushButton(self)
+    
+        self.button_test.clicked.connect(lambda: stacked.setCurrentIndex(1))
+        self.button_graph.clicked.connect(lambda: stacked.setCurrentIndex(2))
+        self.button_graph.clicked.connect(lambda: print(self.frameSize()))
 
-        #self.frame1.pack(fill="both", expand=True)
-
-        self._frame = None
-        self.switch_frame(StartPage)
-        
-    def switch_frame(self, frame_class):
-        """Destroys current frame and replaces it with a new one."""
-        new_frame = frame_class(self)
-        if self._frame is not None:
-            self._frame.destroy()
-        self._frame = new_frame
-        self._frame.pack()
-
+        #self.button_test.move(self.frameGeometry()*50//100,200)
+        self.button_graph.move(200,230)
     
 
-class StartPage(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        #self..title("Typing speed app")
-        self.geometry("1280x720")
-        Label(self, text="This is the start page").pack(side="top", fill="x", pady=10)
-        Button(self, text="Open page one", command=lambda: master.switch_frame(PageOne)).pack()
-        Button(self, text="Open page two", command=lambda: master.switch_frame(PageTwo)).pack()
+class TS_Test(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.initUI()
 
-class PageOne(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        Label(self, text="This is page one").pack(side="top", fill="x", pady=10)
-        Button(self, text="Return to start page", command=lambda: master.switch_frame(StartPage)).pack()
+    def initUI(self):
+        self.button_back = QPushButton(self)
+        self.button_back.clicked.connect(lambda: stacked.setCurrentIndex(0))
+        self.button_back.move(500,300)
 
-        self.text = open("paragraphs.txt", "r").read().split("BREAK")
-        
-        self.sample_label = Label(self, text=random.choice(self.text))
-        self.sample_label.place(x=100, y=200)
 
-class PageTwo(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        Label(self, text="This is page two").pack(side="top", fill="x", pady=10)
-        Button(self, text="Return to start page", command=lambda: master.switch_frame(StartPage)).pack()
+class Statistics(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)        
+        self.initUI()
+
+    def initUI(self):
+        self.button_back = QPushButton(self)
+        self.button_back.clicked.connect(lambda: stacked.setCurrentIndex(0))
+        self.button_back.move(400,300)
+    
 
 
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    app = QApplication(sys.argv)
+    mainwin = MainWindow()
+    mainwin.show()
+    sys.exit(app.exec_())
 
 
 
